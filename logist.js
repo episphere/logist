afa5623029bd7f5059ed0f0084848c339b3f9f3d
Jonas Almeida <jalmeida@mathbiol.org>
2020-11-16 23:35:25 -0500

    parse data

--- logist.js
+++ logist.js
@@ -1,9 +1,21 @@
 console.log('logist.js loaded')
 
 logist=function(txt){
-    logist.txt=txt
+    //this.txt=txt
+    if(txt){
+        logist.dt=logist.parse(txt)
+    }
 }
 
 logist.parse=txt=>{
-
+    const tb = txt.split(/\n/g).map(x=>x.split(/\t/g))
+    const dt={}
+    // header
+    dt.cols=tb[0].slice(1)
+    dt.rows=tb.slice(1).map(x=>x[0])
+    dt.x=tb.slice(1).map(x=>x.slice(1,-1))
+    dt.x=dt.x.map(x=>x.map(x=>parseInt(x)))
+    dt.y=tb.slice(1).map(x=>x.slice(-1))
+    dt.y=dt.y.map(x=>parseInt(x[0]))
+    return dt
 }
