c35445070c32dc82209258e99224fdda5d34b503
Jonas Almeida <jalmeida@mathbiol.org>
2020-11-17 22:20:10 -0500

    tabulate

--- logist.js
+++ logist.js
@@ -13,13 +13,27 @@ logist.parse=txt=>{
     const tb = txt.split(/\n/g).map(x=>x.split(/\t/g))
     const dt={}
     // header
-    dt.cols=tb[0].slice(1)
+    dt.cols=tb[0]
     dt.rows=tb.slice(1).map(x=>x[0])
     dt.x=tb.slice(1).map(x=>x.slice(1,-1))
-    dt.x=dt.x.map(x=>x.map(x=>parseInt(x)))
+    dt.x=logist.transpose(dt.x.map(x=>x.map(x=>parseInt(x))))
     dt.y=tb.slice(1).map(x=>x.slice(-1))
     dt.y=dt.y.map(x=>parseInt(x[0]))
+    dt.b=dt.cols.slice(1,-1).map(_=>NaN)
+    dt.min = dt.x.map(xi=>Math.min(...xi))
+    dt.max = dt.x.map(xi=>Math.max(...xi))
     return dt
+}
+
+logist.transpose=M=>{
+    const n = M.length
+    const m = M[0].length
+    let jj = [...Array(n)].map((_,j)=>j)
+    return [...Array(m)].map((_,i)=>{
+        return jj.map((_,j)=>{
+            return M[j][i]
+        })
+    })
 }
 
 logist.ui=(div='logistDiv')=>{
@@ -28,7 +42,36 @@ logist.ui=(div='logistDiv')=>{
     }
     let h = `<p style="color:green">Started: ${Date()}</p>`
     h+='<h2>Logistic regression</h2>'
-    h+=`<p>Dataset: ${logist.dt.cols.length} variables x ${logist.dt.rows.length} observations</p>`
+    h+=`<p>Dataset: ${logist.dt.cols.length-2} variables x ${logist.dt.rows.length} observations</p>`
     h+='<div id="regressionDiv">...</div>'
     div.innerHTML=h
+    let divR=document.getElementById('regressionDiv')
+    divR.innerHTML=''
+    let tb = document.createElement('table')
+    divR.appendChild(tb)
+    let tr0 = document.createElement('tr')
+    tb.appendChild(tr0)
+    let td0 = document.createElement('td')
+    tr0.appendChild(td0)
+    td0.textContent=logist.dt.cols[0]
+    td0.style.textAlign='center'
+    td0.style.color='darkgreen'
+    const hh = ['min','max','b']
+    hh.forEach(x=>{
+        let th = document.createElement('th')
+        tr0.appendChild(th)
+        th.textContent=x
+    })
+    logist.dt.cols.slice(1,-1).forEach((x,i)=>{
+        let tr = document.createElement('tr')
+        tb.appendChild(tr)
+        let th = document.createElement('th')
+        th.textContent=x
+        tr.appendChild(th)
+        hh.forEach(f=>{
+            let td = document.createElement('td')
+            tr.appendChild(td)
+            td.textContent=logist.dt[f][i]
+        })
+    })
 }
