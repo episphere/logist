1fbaf7b69ed486492cdc0575f7418f39bb01aa73
Jonas Almeida <jalmeida@mathbiol.org>
2020-11-18 22:10:36 -0500

    binary case.control

--- logist.js
+++ logist.js
@@ -14,7 +14,7 @@ logist.parse=txt=>{
     const dt={}
     // header
     dt.cols=tb[0]
-    dt.rows=tb.slice(1).map(x=>x[0])
+    dt.pids=tb.slice(1).map(x=>x[0])
     dt.x=tb.slice(1).map(x=>x.slice(1,-1))
     //dt.x=logist.transpose(dt.x.map(x=>x.map(x=>parseInt(x))))
     dt.x=logist.transpose(dt.x)
@@ -32,6 +32,7 @@ logist.parse=txt=>{
             dt.xx.push((dt.x[i]).map(x=>(x==a)*1))
         })
     })
+    dt.chrs=[...new Set(dt.alleles.map(x=>parseInt(x.match(/^\d+/)[0])))].sort((a,b)=>a>b?1:-1)
     return dt
 }
 
@@ -52,7 +53,7 @@ logist.ui=(div='logistDiv')=>{
     }
     let h = `<p style="color:green">Started: ${Date()}</p>`
     h+='<h2>Logistic regression</h2>'
-    h+=`<p>Dataset: ${logist.dt.cols.length-2} variables x ${logist.dt.rows.length} observations</p>`
+    h+=`<p>${logist.dt.chrs.length} chromossomes : ${logist.dt.cols.length-2} positions # ${logist.dt.alleles.length} x ${logist.dt.pids.length} observations</p>`
     h+='<div id="regressionDiv">...</div>'
     div.innerHTML=h
     let divR=document.getElementById('regressionDiv')
@@ -63,25 +64,35 @@ logist.ui=(div='logistDiv')=>{
     tb.appendChild(tr0)
     let td0 = document.createElement('td')
     tr0.appendChild(td0)
-    td0.textContent=logist.dt.cols[0]
-    td0.style.textAlign='center'
+    td0.textContent=`${logist.dt.chrs.length}:${logist.dt.cols.length-2}#${logist.dt.alleles.length}x${logist.dt.xx[0].length}` //logist.dt.cols[0]
+    //td0.style.textAlign='center'
     td0.style.color='darkgreen'
+    let th01 = document.createElement('th')
+    th01.textContent=logist.dt.y.join('') // outcome
+    tr0.appendChild(th01)
+    /*
     const hh = ['min','max','b']
     hh.forEach(x=>{
         let th = document.createElement('th')
         tr0.appendChild(th)
         th.textContent=x
     })
-    logist.dt.cols.slice(1,-1).forEach((x,i)=>{
+    */
+    logist.dt.alleles.forEach((x,i)=>{
         let tr = document.createElement('tr')
         tb.appendChild(tr)
         let th = document.createElement('th')
         th.textContent=x
         tr.appendChild(th)
+        let td01 = document.createElement('td')
+        td01.textContent=logist.dt.xx[i].join('')
+        tr.appendChild(td01)
+        /*
         hh.forEach(f=>{
             let td = document.createElement('td')
             tr.appendChild(td)
             td.textContent=logist.dt[f][i]
         })
+        */
     })
 }
